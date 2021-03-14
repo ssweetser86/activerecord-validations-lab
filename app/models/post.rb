68159aced617777f5aaf9ba5b03bd767956e29fc
Stephen <stephenjsweets@gmail.com>
2021-03-14 19:27:19 -0400

    commit tree 1351c56842bc3dee073695be32c6ad4163c850e5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :is_clickbait
+
+    def is_clickbait
+        if title.present?
+            if !(title.include?("Won't Believe") || title.include?("Secret") || title.match(/\ATop \d*/) || title.include?("Guess"))
+                errors.add(:title, "must be clickbait!")
+            end
+        end
+    end
 end
